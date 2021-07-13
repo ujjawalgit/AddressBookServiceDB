deae7a5a3347ec0db154d78b7a308d3532cbe786
ujjawalgit <57063174+ujjawalgit@users.noreply.github.com>
2021-07-14 03:36:28 +0530

    Added: create a Address Book Service DB

--- src/AddressBook.sql
+++ src/AddressBook.sql
@@ -1,1 +1,28 @@
 "welcome to address book service"
+
+UC1:create a Address Book Service DB
+mysql> CREATE DATABASE Address Book Service
+    ->
+    -> ^C
+mysql> CREATE DATABASE AddressBookService;
+Query OK, 1 row affected (1.79 sec)
+
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| addressbookservice |
+| classicmodels      |
+| information_schema |
+| mysql              |
+| newschema          |
+| payroll_service    |
+| performance_schema |
+| sakila             |
+| sys                |
+| world              |
++--------------------+
+10 rows in set (0.09 sec)
+
+mysql> USE AddressBookService;
+Database changed
