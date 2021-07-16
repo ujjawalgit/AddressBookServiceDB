5d46bdf3e5996c8ac20e71a2c4cf43b28c781d3d
ujjawalgit <57063174+ujjawalgit@users.noreply.github.com>
2021-07-16 09:02:49 +0530

    Added: create  AddressBook table

--- src/AddressBook.sql
+++ src/AddressBook.sql
@@ -24,3 +24,31 @@ mysql> SHOW DATABASES;
 
 mysql> USE AddressBookService;
 Database changed
+
+mysql> create table AddressBook
+ -> (
+ ->firstName varchar(30) not null,
+ ->lastName varchar(30) not null,
+ ->address varchar(255) not null,
+ ->city varchar(30) not null,
+ ->state varchar(30) not null,
+ ->zip int not null,
+ ->phoneNumber bigint  unique,
+ ->email varchar(60) unique
+ );
+Query OK, 0 rows affected (0.58 sec)
+
+mysql> desc addressbook;
++-------------+--------------+------+-----+---------+-------+
+| Field       | Type         | Null | Key | Default | Extra |
++-------------+--------------+------+-----+---------+-------+
+| firstName   | varchar(30)  | NO   |     | NULL    |       |
+| lastName    | varchar(30)  | NO   |     | NULL    |       |
+| address     | varchar(255) | NO   |     | NULL    |       |
+| city        | varchar(30)  | NO   |     | NULL    |       |
+| state       | varchar(30)  | NO   |     | NULL    |       |
+| zip         | int          | NO   |     | NULL    |       |
+| phoneNumber | bigint       | YES  | UNI | NULL    |       |
+| email       | varchar(100) | YES  | UNI | NULL    |       |
++-------------+--------------+------+-----+---------+-------+
+8 rows in set (0.05 sec)
